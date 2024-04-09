b70037165416a2e72de6e54f0ac5c0d75039475c
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2024-04-09 23:38:31 +0400

    chore: removed dead code

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\QuizCollection;
 use App\Models\Quiz;
-use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
@@ -12,8 +11,6 @@ class QuizController extends Controller
 	{
 		$quizzes = Quiz::with('categories', 'difficultyLevel');
 
-		// return new QuizCollection($quizzes->filter($request->only(['categories', 'levels']))->get());
 		return new QuizCollection($quizzes->filter(request(['categories', 'levels', 'sort']))->get());
-		// return response()->json(request('levels'));
 	}
 }
