b8c42a51920024afd2003c58f252285816d0415e
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2024-04-28 18:17:51 +0400

    style: fixed certain styles not working for gmail

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -4,8 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>Document</title>
+    <title>Quizwiz API</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        .main-container {
+            background-color: #f6f6f6;
+            padding:4rem 0;
+            width: 100%;
+        }
+
+        .container {
+            width:fit-content;
+            height:fit-content;
+            margin:auto;
+            padding:0 8px;
+        }
+        .img-container{
+            width:fit-content;
+            margin:0 auto;
+        }
+        .content {
+            margin:0 auto;
+        }
+        .container h1 {
+            margin-top:2rem;
+            text-align: center;
+        }
+
+        .container p {
+            margin: 1rem 0;
+        }
+
+        a {
+            margin: 1rem auto;
+            display: block;
+            width: fit-content;
+            background-color: #4b69fd;
+            text-align: center;
+            border-radius: 10px;
+            padding: 16px 50px;
+            font-size: 1.2rem;
+            text-decoration: none;
+            font-weight: bold;
+            cursor: pointer;
+            white-space: nowrap;
+        }
+    </style>
 </head>
 <body>
     <div class="main-container">
@@ -14,11 +62,11 @@
                 <div class='img-container'>
                     <img src="{{asset('quizwiz-email-logo.png')}}"/>
                 </div>
-                <h1>{{$headerText}}</h1>
+                <h1 style="color:black">{{$headerText}}</h1>
             </div>
-            <p>Hi {{$user}},</p>
-            <p>{{$text}}</p>
-            <a class="link" href="{{$url}}">{{$buttonText}}</a>
+            <p style="color:black">Hi {{$user}},</p>
+            <p style="color:black">{{$text}}</p>
+            <a style="color:white" href="{{$url}}">{{$buttonText}}</a>
         </div>
     </div>
     </html>
